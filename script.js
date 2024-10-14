5e31e003f85a116da2e0fae5f7bd714fc050df94
raghveerdeol <deolraghveer5@gmail.com>
2024-10-14 18:19:34 +0200

    added capital validation

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
 
 addEventListener('input', (event) => {
-    let inputData = document.getElementById('passwordInput').value;
+    let inputData = document.getElementById('passwordInput').value.trim();
     console.log(inputData);
     lengthValidation(inputData);
+    capitalValidation(inputData);
 });
 
 
+// validation functions 
+
+// length validation function 
 function lengthValidation(element) {
     let ValidationElement = document.getElementById('lengthValidation');
 
@@ -17,4 +21,17 @@ function lengthValidation(element) {
         ValidationElement.classList.remove('tick');
         ValidationElement.classList.add('cross');
     }
-}
+};
+
+// capital validation function
+function capitalValidation(element) {
+    let ValidationElement = document.getElementById('capitalValidation');
+
+    if (element.match(/[A-Z]/g)) {
+        ValidationElement.classList.remove('cross');
+        ValidationElement.classList.add('tick');
+    } else {
+        ValidationElement.classList.remove('tick');
+        ValidationElement.classList.add('cross');
+    }
+};
